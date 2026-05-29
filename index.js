dcca7bd9464685e7f701b523aea9680449d24f00
Milber55 <milbermosqueraubaldo@gmail.com>
2026-05-28 22:16:14 -0500

    Servidor Express

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
 console.log("Hola mundo");
 
